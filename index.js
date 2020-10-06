0dd258f28d033f6dd3dea2f9ee6f759b57baa617
aherredev <aherredev@gmail.com>
2020-10-06 14:44:53 -0500

    Add excercie done v1

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Coladera de Eratostenes v1.0')
+const n = 100
+const elementos = Array(n)
+const limite = Math.sqrt(n)
+let elementoActual;
+elementos[0] = false
+elementos[1] = 2
+for(let i = 2; i < elementos.length; i++){
+  if(i%2 == 0){
+    elementos[i] = i + 1
+  }else {
+    elementos[i] = false
+  }
+}
+for(let i = 2; i< limite; i++){
+  for(let e = i;e < elementos.length; e++){
+    if(elementos[e]%i === 0){
+      elementos[e] = false
+    }
+  }
+}
+console.log('Los que tienen numero son primos')
+console.log(elementos)
